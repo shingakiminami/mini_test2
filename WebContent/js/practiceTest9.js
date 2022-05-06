80e9ffad9bc7ca933a816751ebd01d8d6ba27cb5
m-sagawa <m.shingaki@ks-japan.jp>
2022-05-06 12:33:48 +0900

    ミニテスト

---
+++ WebContent/js/practiceTest9.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+//連想配列で５つのデータを格納
+var items = [
+	{ name:'りんご',price:100,count;3 },
+	{ name:'ぶどう',price:200,count;1 },
+	{ name:'ばなな',price:100,count;1 },
+	{ name:'いちご',price:200,count;2 },
+	{ name:'みかん',price:300,count;3 }
+];
+
+//関数の呼び出し
+higherPrice(250);
+
+//higherPrice関数
+function higherPrice(targetPrice){
+
+
+}
