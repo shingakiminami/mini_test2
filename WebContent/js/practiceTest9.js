1f9a77900aa74137c210edb202b624238557f2ba
m-sagawa <m.shingaki@ks-japan.jp>
2022-05-06 13:33:53 +0900

    ミニテスト問題２以外完了

--- WebContent/js/practiceTest9.js
+++ WebContent/js/practiceTest9.js
@@ -1,13 +1,11 @@
-'use strict';
-
-
 //連想配列で５つのデータを格納
+
 var items = [
-	{ name:'りんご',price:100,count;3 },
-	{ name:'ぶどう',price:200,count;1 },
-	{ name:'ばなな',price:100,count;1 },
-	{ name:'いちご',price:200,count;2 },
-	{ name:'みかん',price:300,count;3 }
+	{ name:'りんご',price:100,count:3 },
+	{ name:'ぶどう',price:200,count:1 },
+	{ name:'ばなな',price:100,count:1 },
+	{ name:'いちご',price:200,count:2 },
+	{ name:'みかん',price:300,count:3 }
 ];
 
 //関数の呼び出し
@@ -16,5 +14,10 @@ higherPrice(250);
 //higherPrice関数
 function higherPrice(targetPrice){
 
+  for (var i=0; i< items.length; i++){
+	if (items[i].price*items[i].count > targetPrice) {
 
+		console.log(items[i].name);
+	}
+  }
 }
